2410fc37d5d31119922299c7c2166fa8882beb64
Alizeh Khan <alizehedinburgh@gmail.com>
2023-01-08 10:52:04 +0100

    feat: react-modal

--- src/components/PromptButton.tsx
+++ src/components/PromptButton.tsx
@@ -1,19 +1,40 @@
+import { format } from 'date-fns'
+import { useState } from 'react'
+import ReactModal from 'react-modal'
+
 import { ReactComponent as Van } from '../images/van.svg'
 
-const PromptButton = () => (
-  <button className="prompt-button">
-    <div className="prompt-button-left">
-      <p>Thu 14 March</p>
-      <div className="prompt-van-label">
-        <Van />
-        <p>Earliest delivery</p>
-      </div>
-    </div>
-    <div className="prompt-button-right">
-      <div className="prompt-calendar">14</div>
-      <p>Change</p>
-    </div>
-  </button>
-)
+const PromptButton = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [deliveryDate, setDeliveryDate] = useState(new Date())
+
+  return (
+    <>
+      <button className="prompt-button" onClick={() => setIsModalOpen(true)}>
+        <div className="prompt-button-left">
+          <p>{format(deliveryDate, 'eee d LLLL')}</p>
+          <div className="prompt-van-label">
+            <Van />
+            <p>Earliest delivery</p>
+          </div>
+        </div>
+        <div className="prompt-button-right">
+          <div className="prompt-calendar">{format(deliveryDate, 'd')}</div>
+          <p>Change</p>
+        </div>
+      </button>
+
+      <ReactModal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        shouldCloseOnOverlayClick
+        className="modal"
+        overlayClassName="overlay"
+      >
+        hello
+      </ReactModal>
+    </>
+  )
+}
 
 export default PromptButton
